60a10abba0d41264fdf0a917017bc8d24142f9be
Hanah <hanahkeenev@gmail.com>
2023-10-13 20:45:24 +0800

    first commit

---
+++ app/src/main/java/com/example/adventours/SigninActivity.java
@@ -0,0 +1,14 @@
+package com.example.adventours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
