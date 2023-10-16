dae1aabc5e9efbf38cb53700d890bd3b29e5fd20
Hanah <hanahkeenev@gmail.com>
2023-10-16 22:25:51 +0800

    Updated file with new changes

---
+++ app/src/main/java/com/example/adventours/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.adventours;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity {
+    public static class homeActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
+        }
+    }
+}
--- app/src/main/java/com/example/adventours/LoginActivity.java
+++ app/src/main/java/com/example/adventours/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.adventours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button button = findViewById(R.id.loginbutton);
+        button.setOnClickListener(view -> openlogin());
     }
-}
+    private void openlogin() {
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+    }
+
+        }
