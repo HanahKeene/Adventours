2130ea3c5c9a7f65e89e693a52a5e2f9c104f276
Trixie <trixielavadiasoriano@gmail.com>
2024-03-20 03:30:04 +0800

    System Updates

--- app/src/main/java/com/example/adventours/toursinfo.java
+++ app/src/main/java/com/example/adventours/toursinfo.java
@@ -4,29 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.adventours.ui.ConfirmationScreen;
+import com.example.adventours.ui.RoomDetails;
 import com.example.adventours.ui.adapters.tourAdapter;
 import com.example.adventours.ui.models.tourModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class toursinfo extends AppCompatActivity {
 
     ImageView cover;
     TextView tourname, accomodation, description, stay, pricefld;
+    ImageButton in_btn, out_btn, roomnumdec, roomnumin;
     RecyclerView inclusionRecyclerView;
+    Button addtoitinerary;
     ImageButton back;
     FirebaseFirestore db;
-
+    TextView priceTxtView, paxTxtView, roomName, amenitiesTxtView, descTxtView, in_date, out_date, roomNumber;
+    Button reservebtn, reservearoom;
+    int currentRoomNumber = 1;
+    final int MIN_ROOM_NUMBER = 1;
+    final int MAX_ROOM_NUMBER = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +62,13 @@ public class toursinfo extends AppCompatActivity {
         pricefld = findViewById(R.id.price);
         description = findViewById(R.id.description);
         stay = findViewById(R.id.stay);
+        addtoitinerary = findViewById(R.id.addtoitinerary);
         inclusionRecyclerView = findViewById(R.id.inclusions);
+
         back = findViewById(R.id.backbtn);
 
         back.setOnClickListener(View -> finish());
+        addtoitinerary.setOnClickListener(View -> openDialog());
 
         db = FirebaseFirestore.getInstance();
 
@@ -50,6 +77,201 @@ public class toursinfo extends AppCompatActivity {
 
         fetchtourDetails(tour_id);
 
+    }
+
+    private void openDialog() {
+
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.reserve_details);
+
+        in_date = findViewById(R.id.in_date);
+        out_date = findViewById(R.id.out_date);
+        in_btn = findViewById(R.id.in_btn);
+        out_btn = findViewById(R.id.out_btn);
+        roomnumin = findViewById(R.id.roomnumin);
+        roomnumdec = findViewById(R.id.roomnumdec);
+        reservearoom = findViewById(R.id.reservearoom);
+
+
+        // Find ImageButtons within the dialog's layout
+        ImageButton in_btn = dialog.findViewById(R.id.in_btn);
+        ImageButton out_btn = dialog.findViewById(R.id.out_btn);
+        ImageButton roomnumin = dialog.findViewById(R.id.roomnumin);
+        ImageButton roomnumdec = dialog.findViewById(R.id.roomnumdec);
+
+        TextView in_date = dialog.findViewById(R.id.in_date);
+        TextView out_date = dialog.findViewById(R.id.out_date);
+        TextView roomNumber = dialog.findViewById(R.id.room_num);
+
+        Calendar currentDateTime = Calendar.getInstance();
+        String currentDate = formatDateAsWords(currentDateTime.get(Calendar.YEAR), currentDateTime.get(Calendar.MONTH), currentDateTime.get(Calendar.DAY_OF_MONTH));
+        in_date.setText(currentDate);
+        out_date.setText(currentDate);
+
+        Button reservearoom = dialog.findViewById(R.id.reservearoom);
+
+        in_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a custom popup window
+                showDatePicker(in_date);
+            }
+        });
+
+        out_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a custom popup window
+                showDatePicker(out_date);
+            }
+        });
+
+        roomNumber.setText(String.valueOf(currentRoomNumber));
+
+        // Decrease the room number when the decrement button is clicked
+        roomnumdec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentRoomNumber > MIN_ROOM_NUMBER) {
+                    currentRoomNumber--;
+                    roomNumber.setText(String.valueOf(currentRoomNumber));
+                }
+            }
+        });
+
+        // Increase the room number when the increment button is clicked
+        roomnumin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentRoomNumber < MAX_ROOM_NUMBER) {
+                    currentRoomNumber++;
+                    roomNumber.setText(String.valueOf(currentRoomNumber));
+                }
+            }
+        });
+
+        reservearoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(toursinfo.this, ConfirmationScreen.class);
+
+                // Add any data you want to pass to the ConfirmationScreen activity
+                // For example, you can use putExtra to pass room details
+                Intent intent1 = getIntent();
+                String hotelId = intent1.getStringExtra("HotelId");
+                String roomId = intent1.getStringExtra("RoomId");
+
+                intent.putExtra("HotelId", hotelId);
+                intent.putExtra("Room ID", roomId);
+
+                Toast.makeText(toursinfo.this, "Hotel ID: " + hotelId + " ROOM ID: " + roomId, Toast.LENGTH_SHORT).show();
+
+                intent.putExtra("RoomQuantity", roomNumber.getText().toString());
+                intent.putExtra("RoomName", roomName.getText().toString());
+                intent.putExtra("Price", priceTxtView.getText().toString());
+                intent.putExtra("CheckIn", in_date.getText().toString());
+                intent.putExtra("CheckOut", out_date.getText().toString());
+
+                // Add more data as needed
+
+                startActivity(intent);
+
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (currentUser != null) {
+                    String userId = currentUser.getUid();
+
+                    // Fetch user details from Firestore using the obtained userId
+                    DocumentReference userRef = db.collection("Users").document(userId);
+
+                    userRef.get().addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            // Retrieve user details
+                            String firstName = documentSnapshot.getString("firstName");
+                            String lastName = documentSnapshot.getString("lastName");
+                            String city = documentSnapshot.getString("city");
+                            String phone = documentSnapshot.getString("phone");
+
+                            // Pass user details to the ConfirmationScreen activity
+                            intent.putExtra("FirstName", firstName);
+                            intent.putExtra("LastName", lastName);
+                            intent.putExtra("City", city);
+                            intent.putExtra("Phone", phone);
+                            intent.putExtra("UserID", userId);
+
+
+                            Toast.makeText(toursinfo.this, "User:" + userId, Toast.LENGTH_SHORT).show();
+                            // Start the ConfirmationScreen activity
+//                            startActivity(intent);
+                        } else {
+                            // Handle the case where the user document does not exist
+                            Toast.makeText(toursinfo.this, "User details not found", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(e -> {
+                        // Handle errors that occurred while fetching user details
+                        Toast.makeText(toursinfo.this, "Error fetching user details", Toast.LENGTH_SHORT).show();
+                    });
+                } else {
+                    // Handle the case where the user is not authenticated
+                    Toast.makeText(toursinfo.this, "User not authenticated", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Window window = dialog.getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+            layoutParams.copyFrom(window.getAttributes());
+            layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            layoutParams.gravity = Gravity.BOTTOM;
+
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            layoutParams.windowAnimations = R.style.DialogAnimation;
+
+            window.setAttributes(layoutParams);
+        }
+
+        dialog.show();
+    }
+
+    private void showDatePicker(TextView inDate) {
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // Format the date as words (e.g., "June 12, 2023")
+                String formattedDate = formatDateAsWords(year, month, day);
+
+                // Set the formatted date to the TextView
+                inDate.setText(formattedDate);
+            }
+        }, year, month, day);
+
+        // Set the minimum date to the current date
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+
+        datePickerDialog.show();
+    }
+
+    private String formatDateAsWords(int year, int month, int day) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        // Create a SimpleDateFormat with the desired format
+        SimpleDateFormat sdf = new SimpleDateFormat("MMMM d, yyyy", Locale.getDefault());
+
+        // Format the date and return the result
+        return sdf.format(calendar.getTime());
     }
 
     private void fetchtourDetails(String tourId) {
